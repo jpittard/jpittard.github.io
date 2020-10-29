0cb9f87796da5c67242f2e8b99fc466803487db4
jpittard <jpittard@hotmail.com>
2020-10-29 11:44:21 -0400

    Added background process

--- posts/redirect_running_process.sh
+++ posts/redirect_running_process.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Background the process
+ctrl-z
+bg %1
+disown -h %1
+ctrl-d
+
 # from https://stackoverflow.com/questions/593724/redirect-stderr-stdout-of-a-process-after-its-been-started-using-command-lin
 
 pid=$(cat /var/run/app/app.pid)
