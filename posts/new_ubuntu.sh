b1ccdeeb743ffb7b9e5e42ac5b6750a295ff4e6d
jpittard <jpittard@hotmail.com>
2022-05-06 14:21:12 -0400

    Update new_ubuntu.sh

--- posts/new_ubuntu.sh
+++ posts/new_ubuntu.sh
@@ -40,7 +40,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Installs
-sudo apt-get install zip podman postgresql-client
+sudo apt-get install zip podman postgresql-client plocate
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
