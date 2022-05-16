9b455754d2eb31756a2acbcd2dc529004569122b
jpittard <jpittard@hotmail.com>
2022-05-16 08:17:50 -0400

    git bundle with array of repos

--- posts/git_bundle.sh
+++ posts/git_bundle.sh
@@ -1,11 +1,26 @@
-# Bundle a full git repo for sneakernet
+# Do a complete, portable extract of git repos and history
+mkdir ~/bundles
+cd ~/bundles
 
 function bundle() {
   repo=$1
-  git clone --mirror git@bitbucket.org:dsanara/${repo}.git 
+  echo $repo
+  git clone --mirror git@bitbucket.org:drms-middleware/${repo}.git 
   cd ${repo}.git
   git bundle create ../${repo}.bundle --all
-  cd ..
+  cd ~/bundles
 }
 
-bundle the-repo
+declare -a -x repos=(
+alf_metadata_migration
+test-pipeline
+and-so-forth
+)
+
+printf "%s\n" "${repos[@]}"
+for repo in ${repos[@]}; do
+  echo "${repo}"
+  bundle "${repo}"
+done
+
+zip --encrypt drms_git_bundles.zip *.bundle
